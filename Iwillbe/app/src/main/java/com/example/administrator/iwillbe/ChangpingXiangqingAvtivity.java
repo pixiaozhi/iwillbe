6b235c98d65bf443338135cb6ee7b72b638c083b
puzuhui <2753015604@qq.com>
2016-12-15 14:19:28 +0800

    no message

--- Iwillbe/app/src/main/java/com/example/administrator/iwillbe/ChangpingXiangqingAvtivity.java
+++ Iwillbe/app/src/main/java/com/example/administrator/iwillbe/ChangpingXiangqingAvtivity.java
@@ -10,5 +10,6 @@ public class ChangpingXiangqingAvtivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_changpingxiangqing);
     }
 }
